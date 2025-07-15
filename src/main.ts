ba6c7c2928f44bf42f9f4c1d67cff9f46e8f205b
Sehajkaur24 <sehajkaur9155@gmail.com>
2025-07-15 16:56:48 +0530

    feat: added CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // ✅ Enable CORS so frontend can access backend
+  app.enableCors({
+    origin: 'http://localhost:3000', // allow your frontend
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Book Database API')
     .setDescription('Book Database API description')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
