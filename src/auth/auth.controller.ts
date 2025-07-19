5b85f18b33e4941c53f698f645b98d32f5f8e981
Harmanpreet Singh <harmanpreet181001@gmail.com>
2025-07-19 17:02:49 +0530

    feat: sign in api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignUpDto } from './dto/sign-up.dto';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   register(@Body() data: SignUpDto) {
     return this.authService.register(data);
+  }
+
+  @Post('login')
+  login(@Body() data: SignInDto) {
+    return this.authService.login(data);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/database/entities/user.entity';
 import { Repository } from 'typeorm';
 import { SignUpDto } from './dto/sign-up.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   async register(data: SignUpDto): Promise<User> {
@@ -16,5 +18,18 @@ export class AuthService {
     user.email = data.email;
     user.password = data.password;
     return await this.userRepository.save(user);
+  }
+
+  async login(data: SignUpDto): Promise<{ user: User; token: string }> {
+    const user = await this.userRepository.findOneBy({
+      email: data.email,
+    });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return {
+      token: await this.jwtService.signAsync({ id: user.id }),
+      user,
+    };
   }
 }
